45b243ac0f4cfff12d906276c3a4d68717e01e28
Marisol Garza <marisolg955@gmail.com>
2022-10-07 18:49:45 -0500

    commit tree b524dbb7e00da900360541555177a8348cb01d31

---
+++ index.js
@@ -0,0 +1,7 @@
+function introduction(value1) {
+    return `Hi, my name is ${value1}.`
+}
+function introductionWithLanguage(value1, language) {
+    return `Hi, my name is ${value1} and i am learning to program in ${language}.`;
+
+}
